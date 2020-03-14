24d79acf88e2eae5d040a8c46ac5499fe2941be4
Patryk Bartkowiak <patrykbart99@gmail.com>
2020-03-14 22:49:13 +0100

    It is executing last email message in terminal

--- app.py
+++ app.py
@@ -1,28 +1,42 @@
 import imaplib
 import email
+import os
 
-print("----------GMAIL COMMANDER----------")
+print("----------GMAIL COMMANDER----------\n")
 user = input("E-mail: ")
 password = input("Password: ")
 imap_url = "imap.gmail.com"
+
+print()
 
 mail = imaplib.IMAP4_SSL(imap_url)
 mail.login(user, password)
 mail.select("INBOX")
 
-result, data = mail.uid("search", None, "ALL")
-inbox_item_list = data[0].split()
+old_message = ""
+current_message = ""
 
-most_recent = inbox_item_list[-1]
+while True:
+    result, data = mail.uid("search", None, "ALL")
+    inbox_item_list = data[0].split()
 
-result2, email_data = mail.uid("fetch", most_recent, "(RFC822)")
-raw_email = email_data[0][1].decode("utf-8")
-email_message = email.message_from_string(raw_email)
+    most_recent = inbox_item_list[-1]
 
-for part in email_message.walk():
-    if part.get_content_maintype() == "multipart":
-        continue
-    filename = part.get_filename()
-    content_type = part.get_content_type()
-    if "plain" in content_type:
-        print(part.get_payload())
+    result2, email_data = mail.uid("fetch", most_recent, "(RFC822)")
+    raw_email = email_data[0][1].decode("utf-8")
+    email_message = email.message_from_string(raw_email)
+
+    for part in email_message.walk():
+        if part.get_content_maintype() == "multipart":
+            continue
+        filename = part.get_filename()
+        content_type = part.get_content_type()
+
+        if "plain" in content_type and email_message["Subject"] == "command":
+            current_message = part.get_payload()
+
+        if old_message != current_message:
+            os.system(current_message[:-2])
+            print("\n" + 100 * "-" + "\n")
+            old_message = current_message
+
